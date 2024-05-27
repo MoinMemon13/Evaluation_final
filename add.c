8da873007140598f7381b12d3b9824de198c6446
Moin <fm0746446@gmail.com>
2024-05-27 21:22:16 +0530

    first commit

--- add.c
+++ add.c
@@ -7,11 +7,11 @@ int main() {
     scanf("%d", &num1);
     
     printf("Enter the second number: ");
-    scanf("%d", &num2);
+    scanf("%d", &num3);
     
-    sum = num1 + num2;
+    sum = num1 + num3;
     
-    printf("The sum of %d and %d is %d.", num1, num2, sum);
+    printf("The sum of %d and %d is %d.", num1, num3, sum);
     
     return 0;
 }
