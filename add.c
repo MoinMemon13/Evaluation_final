96c251e7e49fa5d543d9399dd919b687b167a175
Mohammed Moin H Memon <157345294+MoinMemon13@users.noreply.github.com>
2024-05-27 21:26:39 +0530

    Update add.c

--- add.c
+++ add.c
@@ -7,9 +7,9 @@ int main() {
     scanf("%d", &num1);
     
     printf("Enter the second number: ");
-    scanf("%d", &num2);
+    scanf("%d", &num3);
     
-    sum = num1 + num2;
+    sum = num1 + num3;
     
     printf("The sum of %d and %d is %d.", num1, num2, sum);
     
